e1f28c43ec6491125d043c57e1b3c46050b27682
Swapnil Singh <swapnil@embibe.com>
2014-06-14 17:03:13 +0530

    adds cloudy background

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-var k           = false;
-var j           = false;
+'use strict';
+
+var $           = Snap.select;
+var $$          = Snap.selectAll;
 var i           = 0;
 var ang         = 0;
-var looking     = null;
 var scrollTimer = null;
 var danceTimer  = null;
 var lookTimer   = null;
+var initSkate   = null;
 var dancing     = null;
 var danceDelay  = 5000;
 var prevScrollX = 0;
 var foodPoints  = [];
 var windowWidth = window.outerWidth;
 
-var cloud       = Snap.select('.cloud');
-var fancypants  = Snap.select('.fp');
-var hair        = Snap.select('#Hair');
-var head        = Snap.select('#Face');
-var eyes        = Snap.selectAll('#Right_eye,#Left_eye');
-var smile       = Snap.select('#Smile');
-var mouth       = Snap.select('#Open_mouth_1_');
-var spine       = Snap.select('#Spine');
-var hand1       = Snap.select('#Left_hand');
-var hand2       = Snap.select('#Right_hand');
-var belt        = Snap.select('#Belt_1_');
-var pant1       = Snap.select('#Left_leg');
-var pant2       = Snap.select('#Right_leg');
-var wheels      = Snap.selectAll('#wheel,#wheel_1_');
-var legCrack    = Snap.select('#Leg_crack');
-var leftFolds   = Snap.select('#Left_pant_folds');
-var rightFolds  = Snap.select('#Right_pant_folds');
-var pants       = Snap.selectAll('#Right_leg,#Left_leg');
-var wheels      = Snap.selectAll('#wheel,#wheel_1_');
-var eatables    = Snap.selectAll('.eatable');
+var cloud       = $('.cloud');
+var fancypants  = $('.fp');
+var hair        = $('#Hair');
+var head        = $('#Face');
+var eyes        = $$('#Right_eye,#Left_eye');
+var smile       = $('#Smile');
+var mouth       = $('#Open_mouth_1_');
+var spine       = $('#Spine');
+var hand1       = $('#Left_hand');
+var hand2       = $('#Right_hand');
+var belt        = $('#Belt_1_');
+var pant1       = $('#Left_leg');
+var pant2       = $('#Right_leg');
+var wheels      = $$('#wheel,#wheel_1_');
+var legCrack    = $('#Leg_crack');
+var leftFolds   = $('#Left_pant_folds');
+var rightFolds  = $('#Right_pant_folds');
+var pants       = $$('#Right_leg,#Left_leg');
+var wheels      = $$('#wheel,#wheel_1_');
+var eatables    = $$('.eatable');
 
 var pivots = {
   'spine':[spine.attr('x1'),spine.attr('y1')],
@@ -51,10 +53,10 @@ var init = function(){
 
   setTimeout(function(){
     lookLeft();
-    initSKate = setInterval(function(){skate('front');},20);
+    initSkate = setInterval(function(){skate('front');},20);
     fancypants.attr('class','fp');
     setTimeout(function(){
-      clearTimeout(initSKate);
+      clearTimeout(initSkate);
       lookStraight();
       setTimeout(blinkEyes,1000);
     },1500);
@@ -109,17 +111,18 @@ function onScroll(){
     scrollContent(window.pageXOffset);
   }, 1);
 
-  if(lookTimer)
+  if(lookTimer) {
     clearTimeout(lookTimer);
+  }
 
   lookTimer = setTimeout(lookStraight,100);
-};
+}
 
 var listItems = document.querySelectorAll('.story li');
 
 function scrollContent(pos){
   var activeIndex = Math.floor(pos/windowWidth)+1;
-  for ( i in listItems) {
+  for(var i in listItems) {
     listItems[i].className = '';
   }
   document.querySelector('.story li:nth-child('+activeIndex+')').className = 'active';
@@ -127,13 +130,13 @@ function scrollContent(pos){
 
 function skate(dir){
   wheels.forEach(function(el){
-    ang = (dir == 'front' ? ang+1 : ang-1);
-    el.transform("r"+[10*ang]);
+    ang = (dir === 'front' ? ang+1 : ang-1);
+    el.transform('r'+[10*ang]);
   });
 }
 
 function moveHair(angle){
-  hair.animate({transform: "r"+[angle,pivots.hair]},10);
+  hair.animate({transform: 'r'+[angle,pivots.hair]},10);
 }
 
 function eat(){
@@ -155,11 +158,11 @@ function eat(){
 
 function blinkEyes(){
   eyes.forEach(function(el){
-    el.attr({"r":0});
+    el.attr({'r':0});
   });
   setTimeout(function(){
     eyes.forEach(function(el){
-      el.attr({"r":4.08});
+      el.attr({'r':4.08});
     });
   },100);
 }
@@ -172,69 +175,69 @@ function changePants(color){
 
 function lookLeft(){
   eyes.forEach(function(el){
-    el.animate({transform:"t"+[10,0]},50);
+    el.animate({transform:'t'+[10,0]},50);
   });
 }
 
 function lookRight(){
   eyes.forEach(function(el){
-    el.animate({transform:"t"+[-10,0]},50);
+    el.animate({transform:'t'+[-10,0]},50);
   });
 }
 
 function openMouth(){
   smile.attr('display','none');
-  mouth.attr('display','inline').animate({transform:"t"+[-26,0]},20);//.attr({mask: head})
+  mouth.attr('display','inline').animate({transform:'t'+[-26,0]},20);
 }
 
 function closeMouth(){
   smile.attr('display','inline');
-  mouth.attr('display','none').animate({transform:"t"+[0,0]},20);
+  mouth.attr('display','none').animate({transform:'t'+[0,0]},20);
 }
 
 function lookStraight(){
   eyes.forEach(function(el){
-    el.animate({transform:"t"+[0,0]},50);
+    el.animate({transform:'t'+[0,0]},50);
   });
 }
 
 function moveClouds() {
   setInterval(function () {
-    cloud.transform("t"+[i++,0]);
+    cloud.transform('t'+[i++,0]);
   },200);
 }
 
 function dance(){
-  pant1.animate({transform:"t"+[-7,0]},200);
-  pant2.animate({transform:"t"+[-7,0]},200);
-  spine.animate({transform:"r"+[5,pivots.spine]},200);
-  legCrack.animate({transform:"t"+[-7,0]+"r"+[-10,pivots.folds]},200);
-  leftFolds.animate({transform:"t"+[-7,0]+"r"+[-10,pivots.folds]},200);
-  rightFolds.animate({transform:"t"+[-7,0]+"r"+[-10,pivots.folds]},200);
-  hand1.animate({transform:"r"+[-7,pivots.spine]},200);
-  hand2.animate({transform:"r"+[-7,pivots.spine]},200);
-  belt.animate({transform:"t"+[-7,0]},200);
+  pant1.animate({transform:'t'+[-7,0]},200);
+  pant2.animate({transform:'t'+[-7,0]},200);
+  spine.animate({transform:'r'+[5,pivots.spine]},200);
+  legCrack.animate({transform:'t'+[-7,0]+'r'+[-10,pivots.folds]},200);
+  leftFolds.animate({transform:'t'+[-7,0]+'r'+[-10,pivots.folds]},200);
+  rightFolds.animate({transform:'t'+[-7,0]+'r'+[-10,pivots.folds]},200);
+  hand1.animate({transform:'r'+[-7,pivots.spine]},200);
+  hand2.animate({transform:'r'+[-7,pivots.spine]},200);
+  belt.animate({transform:'t'+[-7,0]},200);
   setTimeout(function(){
-    pant1.animate({transform:"t"+[7,0]},200);
-    pant2.animate({transform:"t"+[7,0]},200);
-    spine.animate({transform:"r"+[-5,pivots.spine]},200);
-    legCrack.animate({transform:"t"+[7,0]+"r"+[15,pivots.folds]},200);
-    leftFolds.animate({transform:"t"+[7,0]+"r"+[15,pivots.folds]},200);
-    rightFolds.animate({transform:"t"+[7,0]+"r"+[15,pivots.folds]},200);
-    hand1.animate({transform:"r"+[7,pivots.spine]},200);
-    hand2.animate({transform:"r"+[7,pivots.spine]},200);
-    belt.animate({transform:"t"+[7,0]},200);
+    pant1.animate({transform:'t'+[7,0]},200);
+    pant2.animate({transform:'t'+[7,0]},200);
+    spine.animate({transform:'r'+[-5,pivots.spine]},200);
+    legCrack.animate({transform:'t'+[7,0]+'r'+[15,pivots.folds]},200);
+    leftFolds.animate({transform:'t'+[7,0]+'r'+[15,pivots.folds]},200);
+    rightFolds.animate({transform:'t'+[7,0]+'r'+[15,pivots.folds]},200);
+    hand1.animate({transform:'r'+[7,pivots.spine]},200);
+    hand2.animate({transform:'r'+[7,pivots.spine]},200);
+    belt.animate({transform:'t'+[7,0]},200);
   }, 250);
 }
 
 function resetFancyPants(){
-  pant1.animate({transform:"t"+[0,0]},200);
-  pant2.animate({transform:"t"+[0,0]},200);
-  spine.animate({transform:"r"+[0,pivots.spine]},200);
-  legCrack.animate({transform:"t"+[0,0]+"r"+[0,pivots.folds]},200);
-  leftFolds.animate({transform:"t"+[0,0]+"r"+[0,pivots.folds]},200);
-  rightFolds.animate({transform:"t"+[0,0]+"r"+[0,pivots.folds]},200);
-  hand1.animate({transform:"r"+[0,pivots.spine]},200);
-  hand2.animate({transform:"r"+[0,pivots.spine]},200);
-  belt.animate({transform:"t"+[0,0]},200);
+  pant1.animate({transform:'t'+[0,0]},200);
+  pant2.animate({transform:'t'+[0,0]},200);
+  spine.animate({transform:'r'+[0,pivots.spine]},200);
+  legCrack.animate({transform:'t'+[0,0]+'r'+[0,pivots.folds]},200);
+  leftFolds.animate({transform:'t'+[0,0]+'r'+[0,pivots.folds]},200);
+  rightFolds.animate({transform:'t'+[0,0]+'r'+[0,pivots.folds]},200);
+  hand1.animate({transform:'r'+[0,pivots.spine]},200);
+  hand2.animate({transform:'r'+[0,pivots.spine]},200);
+  belt.animate({transform:'t'+[0,0]},200);
 }
